cda677e7ded495baccbf83440143f64b86bdfd9a
Utsav Solanki <30657412+US99123@users.noreply.github.com>
2023-09-12 18:03:14 +0530

    files added
    main and var file added

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "access_key" {
+    description = "The AWS access key."
+    default = "******"
+}
+
+variable "secret_key" {
+    description = "The AWS secret key."
+    default = "******"
+}
+
+variable "region" {
+    description = "The AWS region."
+    default = "us-east-1"
+}
